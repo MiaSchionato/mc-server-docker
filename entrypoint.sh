bbbc08e2e868a593d95b7da6a5479bf4e8404452
Miguel Schionato <migueloliveira642@hotmail.com>
2026-01-17 17:38:04 +0100

    fixed initial script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ if [ ! -f '.env' ]; then
     exit 1
 fi
 
-if [ ! -f 'server.jar']; then
+if [ ! -f 'server.jar' ]; then
     curl -o server.jar $MC_VERSION_URL
 fi
 
-if [ ! -f 'eula.txt']; then
+if [ ! -f 'eula.txt' ]; then
     echo 'eula=true' > eula.txt
 fi
 
